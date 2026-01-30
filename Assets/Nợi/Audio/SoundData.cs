5f79e39beba460580e41112565e650abca75d0cb
Lemon0872 <togialoi000@gmail.com>
2026-01-31 05:32:29 +0700

    tùm lum hết á
    làm lại script tt mới thay cho pltt,

--- Assets/Nợi/Audio/SoundData.cs
+++ Assets/Nợi/Audio/SoundData.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 [CreateAssetMenu(menuName = "Audio/Sound Data")]
 public class SoundData : ScriptableObject
@@ -6,6 +7,7 @@ public class SoundData : ScriptableObject
     public SoundID soundID;
     public SoundType type;
     public AudioClip clip;
+    public AudioMixer audioMixer;
 
     [Range(0f, 1f)] public float volume = 1f;
     [Range(0.5f, 2f)] public float pitch = 1f;
